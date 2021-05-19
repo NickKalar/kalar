90b0359d3bc2e5870249fa84c73a3cbed005833c
Nicholas <nick@kalar.codes>
2021-05-18 23:14:20 -0400

    initial commit

--- kalar/settings.py
+++ kalar/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import mimetypes
 import django_heroku
-import constants
+import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = constants.SECRET_KEY
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = constants.DEBUG
+DEBUG = local_settings.DEBUG
 
 ALLOWED_HOSTS = ['www.kalar.codes', '.localhost', '127.0.0.1',]
 
